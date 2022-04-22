585d35baa43a5031f6959992e58d383e78635436
Dougal Seeley <git@dougalseeley.com>
2022-04-22 21:59:14 +0100

    Remove unused xml_to_dict and dict_to filters

--- _dependencies/filter_plugins/custom.py
+++ _dependencies/filter_plugins/custom.py
@@ -65,91 +65,6 @@ def xpath(xmlstr, xpath, namespaces=None):
     return [xpath_elem if type(xpath_elem) is str else etree.tostring(xpath_elem).decode() for xpath_elem in xpath_res]
 
 
-# From https://stackoverflow.com/a/10077069/12491741
-# Converts XML to dict using the 'official' (reversible) spec in http://www.xml.com/pub/a/2006/05/31/converting-between-xml-and-json.html
-# Note: doesn't handle namespaces.
-def xml_to_dict(xmlstr):
-    from collections import defaultdict
-    from xml.etree import ElementTree as ET
-    import xmltodict
-
-    # return xmltodict.parse(xmlstr)
-
-    def etree_to_dict(t):
-        d = {t.tag: {} if t.attrib else None}
-        children = list(t)
-        if children:
-            dd = defaultdict(list)
-            for dc in map(etree_to_dict, children):
-                for k, v in dc.items():
-                    dd[k].append(v)
-            d = {t.tag: {k: v for k, v in dd.items()}}  # Everything is a python list, because we can't tell whether an XML element sometimes might defined more than once...
-            # d = {t.tag: {k: v[0] if len(v) == 1 else v for k, v in dd.items()}}
-        if t.attrib:
-            d[t.tag].update(('@' + k, v) for k, v in t.attrib.items())
-        if t.text:
-            text = t.text.strip()
-            if children or t.attrib:
-                if text:
-                    d[t.tag]['#text'] = text
-            else:
-                d[t.tag] = text
-        return d
-
-    return etree_to_dict(ET.fromstring(xmlstr))
-
-
-# From https://stackoverflow.com/a/10077069/12491741
-# Converts dict back to XML
-def dict_to_xml(d):
-    from xml.etree import ElementTree as ET
-    try:
-        basestring
-    except NameError:  # python3
-        basestring = str
-
-    # https://stackoverflow.com/a/65808327/12491741
-    def _pretty_print(current, parent=None, index=-1, depth=0, space="    "):
-        for i, node in enumerate(current):
-            _pretty_print(node, current, i, depth + 1)
-        if parent is not None:
-            if index == 0:
-                parent.text = '\n' + (space * depth)
-            else:
-                parent[index - 1].tail = '\n' + (space * depth)
-            if index == len(parent) - 1:
-                current.tail = '\n' + (space * (depth - 1))
-
-    def _to_etree(d, root):
-        if not d:
-            pass
-        elif isinstance(d, basestring):
-            root.text = d
-        elif isinstance(d, dict):
-            for k, v in d.items():
-                assert isinstance(k, basestring)
-                if k.startswith('#'):
-                    assert k == '#text' and isinstance(v, basestring)
-                    root.text = v
-                elif k.startswith('@'):
-                    assert isinstance(v, basestring)
-                    root.set(k[1:], v)
-                elif isinstance(v, list):
-                    for e in v:
-                        _to_etree(e, ET.SubElement(root, k))
-                else:
-                    _to_etree(v, ET.SubElement(root, k))
-        else:
-            raise TypeError('invalid type: ' + str(type(d)))
-
-    assert isinstance(d, dict) and len(d) == 1
-    tag, body = next(iter(d.items()))
-    node = ET.Element(tag)
-    _to_etree(body, node)
-
-    _pretty_print(node)
-    return ET.tostring(node).decode()
-
 
 class FilterModule(object):
     def filters(self):
@@ -157,7 +72,5 @@ class FilterModule(object):
             'dict_agg': dict_agg,
             'extravars_from_dict': extravars_from_dict,
             'xpath': xpath,
-            'tochr': tochr,
-            'xml_to_dict': xml_to_dict,
-            'dict_to_xml': dict_to_xml
+            'tochr': tochr
         }
