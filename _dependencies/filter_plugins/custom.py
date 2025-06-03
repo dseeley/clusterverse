e4eb6f8be2066d9e512648db6d0abc47c347bae8
Dougal Seeley <git@dougalseeley.com>
2025-06-03 05:44:07 +0100

    Fix race condition in libvirt where an xml_desc is absent due to another process.

--- _dependencies/filter_plugins/custom.py
+++ _dependencies/filter_plugins/custom.py
@@ -6,19 +6,6 @@ from io import BytesIO
 
 display = Display()
 
-def dict2items_recursive(data, key_name='key', value_name='value'):
-    result = []
-    def traverse_dict(d, path):
-        for key, value in d.items():
-            current_path = f"{path}.{key}" if path else key
-            if isinstance(value, dict):
-                traverse_dict(value, current_path)
-            else:
-                result.append({key_name: f"{current_path}", value_name: value})
-
-    traverse_dict(data, "")
-    return result
-
 
 # Create an aggregation on aggkeys (which may be a nested key) within the dictarr array of dicts.  Differs from the builtin jinja2 filter 'groupby' in that it returns a dict for each aggregation, rather than putting it at array elem 0.
 def dict_agg(dictarr, aggkeys):
@@ -68,19 +55,24 @@ def xpath(xmlstr, xpath, namespaces=None):
     if not xpath:
         raise errors.AnsibleFilterError("Error: xpath not defined")
 
-    xml = etree.parse(BytesIO(xmlstr.encode('utf-8')), etree.XMLParser(remove_blank_text=False))
-    # display.warning(u"xml type: %s" % type(xml))
+    if not xmlstr.strip():
+        display.warning(u"xmlstr is empty")
+        return []
+    else:
+        try:
+            xml = etree.parse(BytesIO(xmlstr.encode('utf-8')), etree.XMLParser(remove_blank_text=False))
+        except Exception as e:
+            raise errors.AnsibleFilterError(f"Invalid XML input: {e}")
 
-    ## If this is an array of strings (if the user has parsed out the final leaf node), then return the string; otherwise return the xml.tostring() value (ansible cannot use the native etree Element type anyway)
-    xpath_res = xml.xpath(xpath, namespaces=namespaces, smart_strings=False)
-    return [xpath_elem if type(xpath_elem) is str else etree.tostring(xpath_elem).decode() for xpath_elem in xpath_res]
+        ## If this is an array of strings (if the user has parsed out the final leaf node), then return the string; otherwise return the xml.tostring() value (ansible cannot use the native etree Element type anyway)
+        xpath_res = xml.xpath(xpath, namespaces=namespaces, smart_strings=False)
+        return [xpath_elem if type(xpath_elem) is str else etree.tostring(xpath_elem).decode() for xpath_elem in xpath_res]
 
 
 
 class FilterModule(object):
     def filters(self):
         return {
-            'dict2items_recursive': dict2items_recursive,
             'dict_agg': dict_agg,
             'extravars_from_dict': extravars_from_dict,
             'xpath': xpath,
